eed72db9f7af6e052619350df60ea9eed0680a57
Randy Syring <rsyring@gmail.com>
2010-06-27 00:57:09 -0400

    adjusting setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.1-dev'
+version = '0.1dev'
 
 doc_dir = os.path.join(os.path.dirname(__file__), 'docs')
 index_filename = os.path.join(doc_dir, 'index.txt')
@@ -17,11 +17,14 @@ setup(name='savalidation',
         #'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        ], 
+        ],
       author='Randy Syring',
       author_email='rsyring@gmail.com',
       url='http://bitbucket.org/rsyring/sqlalchemy-validation',
       license='BSD',
       packages=['savalidation'],
       zip_safe=False,
+      install_requires=[
+          'SQLAlchemy>=0.6.1',
+      ],
       )
