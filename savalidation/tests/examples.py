12ec83cde846e450e8081c11e36b356025bca5b0
Randy Syring <randy.syring@level12.io>
2010-07-06 12:22:50 -0400

    - valdiation errors need to be cleared on each validation - fixed problem with validation not working when instance is 	retrieved from the session.  See:
    http://www.sqlalchemy.org/docs/mappers.html#constructors-and-object-initialization

--- savalidation/tests/examples.py
+++ savalidation/tests/examples.py
@@ -12,17 +12,19 @@ engine = sa.create_engine('sqlite://')
 meta = sa.MetaData()
 Base = declarative_base(metadata=meta)
 
-Session = saorm.sessionmaker(
-    bind=engine,
-    autoflush=False,
-    extension=ValidatingSessionExtension()
+Session = saorm.scoped_session(
+    saorm.sessionmaker(
+        bind=engine,
+        autoflush=False,
+        extension=ValidatingSessionExtension()
+    )
 )
 
-sess = Session()
+sess = Session
 
 class Family(Base):
     __tablename__ = 'families'
-    
+
     # SA COLUMNS
     id = sa.Column(sa.Integer, primary_key=True)
     createdts = sa.Column(sa.DateTime, nullable=False, default=datetime.now, server_default=sasql.text('CURRENT_TIMESTAMP'))
@@ -30,7 +32,7 @@ class Family(Base):
     name =  sa.Column(sa.Unicode(75), nullable=False, unique=True)
     reg_num = sa.Column(sa.Integer, nullable=False, unique=True)
     status =  sa.Column(sa.Unicode(15), nullable=False, default=u'active', server_default=u'active')
-    
+
     # VALIDATION
     STATUS_CHOICES = (
         ('active', 'Active'),
@@ -39,14 +41,14 @@ class Family(Base):
     )
     val.validates_constraints()
     val.validates_one_of('status', [k for k, v in STATUS_CHOICES])
-    
+
     #OTHER
     def __str__(self):
         return '<Family id=%s, name=%s>' % (self.id, self.name)
 
 class Person(Base):
     __tablename__ = 'people'
-    
+
     id = sa.Column(sa.Integer, primary_key=True)
     createdts = sa.Column(sa.DateTime, nullable=False, server_default=sasql.text('CURRENT_TIMESTAMP'))
     updatedts = sa.Column(sa.DateTime, onupdate=datetime.now)
@@ -54,7 +56,7 @@ class Person(Base):
     name_last = sa.Column(sa.Unicode(75), nullable=False)
     family_role = sa.Column(sa.Unicode(20), nullable=False)
     nullable_but_required = sa.Column(sa.Unicode(5))
-    
+
     ROLE_CHOICES = (
         ('father', 'Father'),
         ('mother', 'Mother'),
@@ -70,24 +72,24 @@ class IntegerType(Base):
     fld = sa.Column(sa.Integer)
     fld2 = sa.Column(sa.SmallInteger)
     fld3 = sa.Column(sa.BigInteger)
-    
+
     val.validates_constraints()
-    
+
 class NumericType(Base):
     __tablename__ = 'NumericType'
     id = sa.Column(sa.Integer, primary_key=True)
     fld = sa.Column(sa.Numeric)
     fld2 = sa.Column(sa.Float)
-    
+
     val.validates_constraints()
-    
+
 class DateTimeType(Base):
     __tablename__ = 'DateTimeType'
     id = sa.Column(sa.Integer, primary_key=True)
     fld = sa.Column(sa.Date)
     fld2 = sa.Column(sa.DateTime)
     fld3 = sa.Column(sa.Time)
-    
+
     val.converts_date('fld')
     val.converts_datetime('fld2')
     val.converts_time('fld3')
--- savalidation/tests/test_examples.py
+++ savalidation/tests/test_examples.py
@@ -4,13 +4,13 @@ import examples as ex
 from savalidation import ValidationError
 
 class TestFamily(object):
-    
+
     def tearDown(self):
         # need this to clear the session after the exception catching below
         ex.sess.rollback()
         ex.sess.execute('DELETE FROM %s' % ex.Family.__table__)
         ex.sess.commit()
-        
+
     def test_id_is_auto_increment(self):
         f1 = ex.Family(name=u'f1', reg_num=1)
         ex.sess.add(f1)
@@ -19,7 +19,27 @@ class TestFamily(object):
         ex.sess.add(f2)
         ex.sess.commit()
         eq_(f1.id, f2.id - 1)
-    
+
+    def test_edit(self):
+        f1 = ex.Family(name=u'test_edit', reg_num=1)
+        f2 = ex.Family(name=u'test_edit', reg_num=2)
+        ex.sess.add(f1)
+        ex.sess.commit()
+        fid = f1.id
+        ex.sess.remove()
+        f1 = ex.sess.query(ex.Family).get(fid)
+        assert f1.name == 'test_edit'
+        f1.status = 'foobar'
+        try:
+            ex.sess.commit()
+            assert False, 'exception expected'
+        except ValidationError, e:
+            expect = {'status': [u"Value must be one of: active; inactive; moved (not 'foobar')"]}
+            eq_(f1.validation_errors, expect)
+            eq_(str(e), 'validation error(s) on: <Family id=1, name=test_edit>[status]')
+        f1.status = u'inactive'
+        ex.sess.commit()
+
     @raises(saexc.IntegrityError)
     def test_name_is_unique(self):
         f1 = ex.Family(name=u'f', reg_num=1)
@@ -27,7 +47,7 @@ class TestFamily(object):
         ex.sess.add(f1)
         ex.sess.add(f2)
         ex.sess.commit()
-        
+
     @raises(saexc.IntegrityError)
     def test_reg_num_is_unique(self):
         f1 = ex.Family(name=u'f1', reg_num=1)
@@ -35,14 +55,14 @@ class TestFamily(object):
         f2 = ex.Family(name=u'f2', reg_num=1)
         ex.sess.add(f2)
         ex.sess.commit()
-    
+
     def test_status_default(self):
         f1 = ex.Family(name=u'f1', reg_num=1)
         ex.sess.add(f1)
         ex.sess.commit()
         eq_(f1.status, u'active')
         ex.sess.commit()
-    
+
     def test_invalid_status(self):
         try:
             f1 = ex.Family(name=u'f1', reg_num=1, status='foobar')
@@ -53,7 +73,7 @@ class TestFamily(object):
             expect = {'status': [u"Value must be one of: active; inactive; moved (not 'foobar')"]}
             eq_(f1.validation_errors, expect)
             eq_(str(e), 'validation error(s) on: <Family id=None, name=f1>[status]')
-            
+
     def test_missing_regnum(self):
         try:
             f1 = ex.Family(name=u'f1', status=u'active')
@@ -63,7 +83,7 @@ class TestFamily(object):
         except ValidationError, e:
             expect = {'reg_num': [u"Please enter a value"]}
             eq_(f1.validation_errors, expect)
-            
+
     def test_missing_name(self):
         try:
             f1 = ex.Family(reg_num=1, status=u'active')
@@ -73,7 +93,7 @@ class TestFamily(object):
         except ValidationError, e:
             expect = {'name': [u"Please enter a value"]}
             eq_(f1.validation_errors, expect)
-    
+
     def test_multiple_invalid_instances(self):
         try:
             f1 = ex.Family(name='f1', status=u'active')
@@ -88,7 +108,7 @@ class TestFamily(object):
             eq_(f1.validation_errors, expect)
             eq_(f2.validation_errors, expect)
             eq_(str(e), 'validation error(s) on: <Family id=None, name=f1>[reg_num]; <Family id=None, name=f2>[reg_num]')
-            
+
     def test_missing_both(self):
         try:
             f1 = ex.Family()
@@ -100,7 +120,7 @@ class TestFamily(object):
             eq_(len(e.invalid_instances), 1)
             eq_(f1.validation_errors, expect)
             eq_(str(e), 'validation error(s) on: <Family id=None, name=None>[reg_num,name]')
-    
+
     def test_name_too_long(self):
         try:
             f1 = ex.Family(name=u'f1'*100, reg_num=1)
@@ -114,7 +134,7 @@ class TestFamily(object):
 class TestPerson(object):
     def tearDown(self):
         ex.sess.rollback()
-    
+
     def test_id_is_auto_increment(self):
         f1 = ex.Person(name_first=u'f1', name_last=u'l1', family_role=u'father', nullable_but_required=u'f')
         ex.sess.add(f1)
@@ -124,7 +144,7 @@ class TestPerson(object):
         ex.sess.add(f2)
         ex.sess.commit()
         eq_(f1.id, f2.id - 1)
-    
+
     def test_family_role_when_invalid(self):
         try:
             f2 = ex.Person(name_first=u'f1', name_last=u'l1', family_role=u'foobar', nullable_but_required=u'f')
@@ -142,7 +162,7 @@ class TestPerson(object):
             assert False, 'should have been an exception'
         except ValidationError, e:
             assert f2.validation_errors['name_first'][0] == 'Enter a value less than 75 characters long'
-    
+
     def test_nullable_but_required(self):
         # set to None
         try:
@@ -164,11 +184,11 @@ class TestPerson(object):
             eq_(f2.validation_errors, expect)
 
 class TestTypes(object):
-    
+
     def tearDown(self):
         # need this to clear the session after the exception catching below
         ex.sess.rollback()
-    
+
     def test_integer(self):
         inst = ex.IntegerType(fld=10)
         # this None helps test "missing" verse "value not entered"
@@ -186,7 +206,7 @@ class TestTypes(object):
         except ValidationError, e:
             expect = {'fld': [u'Please enter an integer value'], 'fld2': [u'Please enter an integer value'], 'fld3': [u'Please enter an integer value']}
             eq_(inst.validation_errors, expect)
-            
+
     def test_numeric(self):
         inst = ex.NumericType(fld=10.5)
         ex.sess.add(inst)
@@ -202,7 +222,7 @@ class TestTypes(object):
         except ValidationError, e:
             expect = {'fld': [u'Please enter a number'], 'fld2': [u'Please enter a number']}
             eq_(inst.validation_errors, expect)
-            
+
     def test_date_time(self):
         inst = ex.DateTimeType(fld='9/23/2010', fld3='10:25:33 am', fld2='2010-09-26 10:47:35 pm')
         ex.sess.add(inst)
