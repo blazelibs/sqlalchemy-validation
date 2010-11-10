3cb1bbf3b699cc81d7d9497e66b01f9cdbe16c49
Randy Syring <randy.syring@level12.io>
2010-11-10 00:59:23 -0500

    added ip address validator

--- savalidation/tests/test_validators.py
+++ savalidation/tests/test_validators.py
@@ -35,3 +35,33 @@ class TestValidators(object):
             expect = {'minlen': [u'Enter a value at least 20 characters long']}
             eq_(e.invalid_instances[0].validation_errors, expect)
 
+    def test_ip_addr(self):
+        so = ex.SomeObj(ipaddr='127.0.0.1')
+        ex.sess.add(so)
+        ex.sess.commit()
+
+        try:
+            so = ex.SomeObj(ipaddr='127.0.0')
+            ex.sess.add(so)
+            ex.sess.commit()
+            assert False
+        except ValidationError, e:
+            ex.sess.rollback()
+            expect = {'ipaddr': [u'Please enter a valid IP address (a.b.c.d)']}
+            eq_(e.invalid_instances[0].validation_errors, expect)
+
+        # non works b/c its a nullable field
+        so = ex.SomeObj(ipaddr=None)
+        ex.sess.add(so)
+        ex.sess.commit()
+
+        # empty string should not work
+        try:
+            so = ex.SomeObj(ipaddr='')
+            ex.sess.add(so)
+            ex.sess.commit()
+            assert False
+        except ValidationError, e:
+            ex.sess.rollback()
+            expect = {'ipaddr': [u'Please enter a valid IP address (a.b.c.d)']}
+            eq_(e.invalid_instances[0].validation_errors, expect)
