64309262d5dbf5bd50329774f14a51402ffece00
Randy Syring <rsyring@gmail.com>
2010-11-15 19:39:03 -0500

    release 0.1

--- setup.py
+++ setup.py
@@ -1,14 +1,50 @@
+"""
+Introduction
+---------------------
+
+SAValidation Facilitates Active Record like validation on SQLAlchemy declarative model
+objects.
+
+You can install the `in-development version
+<http://bitbucket.org/rsyring/sqlalchemy-validation/get/tip.gz#egg=savlidation-dev>`_
+of savalidation with ``easy_install savalidation==dev``.
+
+The home page is currently the `bitbucket repository
+<http://bitbucket.org/rsyring/sqlalchemy-validation/>`_.
+
+Questions & Comments
+---------------------
+
+Please visit: http://groups.google.com/group/blazelibs
+
+Dependencies
+--------------
+ * SQLAlchemy
+ * FormEncode
+ * Nose (if you want to run the tests)
+
+Credits
+---------
+
+This project borrows code and ideas from:
+
+* `Sqlalchemy Validations <http://code.google.com/p/sqlalchemy-validations/>`_
+* `Elixir <http://elixir.ematia.de/>`_
+
+Current Status
+---------------
+
+The code itself seems stable, but the API is likely to change in the future.
+
+"""
+
 import os
 from setuptools import setup, find_packages
 
-version = '0.1dev'
+from savalidation import VERSION
 
-doc_dir = os.path.join(os.path.dirname(__file__), 'docs')
-index_filename = os.path.join(doc_dir, 'index.txt')
-long_description = open(index_filename).read()
-
-setup(name='savalidation',
-      version=version,
+setup(name='SAValidation',
+      version=VERSION,
       description="Active Record like validation on SQLAlchemy declarative model objects",
       long_description=long_description,
       classifiers=[
@@ -20,7 +56,7 @@ setup(name='savalidation',
         ],
       author='Randy Syring',
       author_email='rsyring@gmail.com',
-      url='http://bitbucket.org/rsyring/sqlalchemy-validation',
+      url='http://bitbucket.org/rsyring/sqlalchemy-validation/',
       license='BSD',
       packages=['savalidation'],
       zip_safe=False,
