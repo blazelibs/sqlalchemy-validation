07a8db3cc3e457b90345af842b7b1ed1b8f0f165
Randy Syring <randy@thesyrings.us>
2014-09-30 17:16:37 -0400

    code standards cleanup

--- savalidation/_internal.py
+++ savalidation/_internal.py
@@ -1,16 +1,10 @@
 import os
-import sys
-from datetime import datetime
-import formencode
-from formencode import Invalid
-import sqlalchemy as sa
-import sqlalchemy.ext.declarative as sadec
-import sqlalchemy.sql as sasql
-import sqlalchemy.orm as saorm
+
 
 def getversion():
     cdir = os.path.abspath(os.path.dirname(__file__))
     return open(os.path.join(cdir, 'version.txt')).read().strip()
+
 
 def is_iterable(possible_iterable):
     if isinstance(possible_iterable, basestring):
@@ -20,3 +14,4 @@ def is_iterable(possible_iterable):
         return True
     except TypeError:
         return False
+
